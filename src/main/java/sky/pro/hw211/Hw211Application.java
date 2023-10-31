e80c0fe4b456fe6b0b579403649ad124cbfd35e2
Vel <korzhavin.vel@yandex.ru>
2023-10-31 19:06:28 +0700

    init

---
+++ src/main/java/sky/pro/hw211/Hw211Application.java
@@ -0,0 +1,13 @@
+package sky.pro.hw211;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw211Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw211Application.class, args);
+	}
+
+}
